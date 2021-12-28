39cfd29889e254fea4de9839c5b54508e886206a
Nathalie Verdavoir <nathalie.verdavoir@laposte.net>
2021-12-28 12:35:48 +0100

    clean home

--- src/pages/IndexOfDrinks.js
+++ src/pages/IndexOfDrinks.js
@@ -1,6 +1,5 @@
 import { useEffect, useState }  from 'react';
 import Header from '../components/Header';
-import Sidebar from '../components/Sidebar';
 import { useParams } from 'react-router-dom';
 import Footer from '../components/Footer';
 import DrinkCard from '../components/DrinkCard';
