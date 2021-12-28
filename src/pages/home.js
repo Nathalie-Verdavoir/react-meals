9b2db65ed8d4874f5ae60dee8fe1a1458e7f10c3
Nathalie Verdavoir <nathalie.verdavoir@laposte.net>
2021-12-28 12:35:08 +0100

    randomDrinkHome  succeed and adapt home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,19 +1,37 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import { RandomDrinkHome } from "../components/RandomDrinkHome";
 import RandomMealHome from "../components/RandomMealHome";
 import Sidebar from "../components/Sidebar";
+import SidebarCategories from "../components/SidebarCategories";
 import SidebarDrinks from "../components/SidebarDrinks";
+import SidebarDrinksCategories from "../components/SidebarDrinksCategories";
+import SidebarDrinksIndex from "../components/SidebarDrinksIndex";
+import SidebarDrinksIngredients from "../components/SidebarDrinksIngredients";
+import SidebarIngredients from "../components/SidebarIngredients";
+import SidebarMeals from "../components/SidebarMeals";
 
 const Home = () => {
     return (
         <>
             <Header/>
             <main className="d-flex col-12">
-                <section className="col-12 col-md-8">
-                <RandomMealHome/>  
+                <section className="col-12 col-md-7 bg-meal p-3">
+                    <h1>Meals</h1>
+                    <RandomMealHome/>  
                 </section>
-                <Sidebar/>
-                <SidebarDrinks/>
+                <section className="col-12 col-md-3 bg-drink p-3">
+                    <h1>Drinks</h1>
+                    <RandomDrinkHome/>  
+                </section>
+                <section className="col-12 col-md-2">
+                    <SidebarCategories/>
+                    <SidebarIngredients/>
+                    <SidebarMeals/>
+                    <SidebarDrinksCategories/>
+                    <SidebarDrinksIngredients/>
+                    <SidebarDrinksIndex/>
+                </section>
             </main>
             
             <Footer/>
