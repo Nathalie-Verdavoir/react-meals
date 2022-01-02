2f9c55ec517ea856482624b21290440aa26818a1
Nathalie Verdavoir <nathalie.verdavoir@laposte.net>
2022-01-02 11:41:32 +0100

    Deploy on another place than site root

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import App from './App';
 import {createStore} from 'redux'
 import rootReducer from './reducers'
 import {Provider} from 'react-redux'
-
+import { createBrowserHistory } from 'history';
 const store = createStore(
   rootReducer 
 )
-
+export const history = createBrowserHistory({
+  basename: process.env.PUBLIC_URL
+});
 ReactDOM.render(
   <React.StrictMode>
      <Provider store={store}> <App /></Provider>
